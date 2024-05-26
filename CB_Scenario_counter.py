b91a26653827018c3b6d30886c92aa3e95fbf0ff
Dhruv <dhruvpatel9242001@gmail.com>
2024-05-26 16:47:40 -0400

    updated

--- CB_Scenario_counter.py
+++ CB_Scenario_counter.py
@@ -77,11 +77,11 @@ class ScenarioProcessor:
 # target_city = 'austin'
 
 #For Dhruv
-source_joy_click_folder = '/mnt/TASI-VRU/click_based_scenarios_joy_csv'
+source_joy_click_folder = '/mnt/TASI-VRU1/click_based_scenarios_joy_csv'
 scenario_dir = '/mnt/TASI-VRU2/Extracted_Click_Based_Scenarios'
 checked_folders_file = '/home/dp75@ads.iu.edu/TASI/TASI-ESCOOTER/CB_Scenario_loaded_in_excel.txt'
-output_file = '/home/dp75@ads.iu.edu/TASI/TASI-ESCOOTER/CB_Scenario_folders_ready_san_diego.txt'
-target_city = 'san_diego'
+output_file = '/home/dp75@ads.iu.edu/TASI/TASI-ESCOOTER/CB_Scenario_folders_ready_boston.txt'
+target_city = 'boston'
 
 processor = ScenarioProcessor(source_joy_click_folder, scenario_dir, checked_folders_file)
 processor.process_folders(target_city)
